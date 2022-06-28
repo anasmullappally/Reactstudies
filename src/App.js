ef343e265eb7130a4cea27e1974fb3d77f65e198
anasmullappally <mohammedanas362@gmail.com>
2022-06-28 16:24:34 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 
 import './App.css';
+import About from './Container/About';
+import Profile from './Container/Profile';
+import React ,{ useState } from 'react'
 
+import { BrowserRouter as Router, Route ,Routes,Link} from "react-router-dom";
 function App() {
+  const [state, setState] = useState('')
+  // let component
+  // if (state === 'about') {
+  //   component = <About></About>
+  // } else if (state === 'profile') {
+  //   component = <Profile></Profile>
+  // }
   return (
     <div className="App">
-     <h1>nhdjgnd</h1>
+      <button onClick={() => setState('about')} >About </button>
+      <button onClick={() => setState('profile')}>Profile</button>
+      <Router >
+        <Link to='/about'> About page</Link>
+        <Link to='/profile'>Profile Page</Link>
+        <Routes>
+        <Route element={<About/>} path='/about' />
+        <Route element={<Profile/>} path='/profile' />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Container/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <h1>About U</h1>
+    </div>
+  )
+}
+
+export default About
